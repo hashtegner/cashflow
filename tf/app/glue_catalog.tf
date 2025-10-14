f597d690c9def99064c1078a8ca02beec3015e8d
Alessandro Tegner <1260049+hashtegner@users.noreply.github.com>
2025-10-14 10:21:19 -0300

    Add refined table

--- tf/app/glue_catalog.tf
+++ tf/app/glue_catalog.tf
@@ -97,87 +97,82 @@ resource "aws_glue_catalog_table" "stocks_raw" {
   }
 }
 
-# resource "aws_glue_catalog_table" "stocks_refined" {
-#   name = "stocks_refined"
-#   database_name = aws_glue_catalog_database.cashflow.name
+resource "aws_glue_catalog_table" "stocks_refined" {
+  name = "stocks_refined"
+  database_name = aws_glue_catalog_database.cashflow.name
 
-#   table_type = "EXTERNAL_TABLE"
+  table_type = "EXTERNAL_TABLE"
 
-#   parameters = {
-#     EXTERNAL              = "TRUE"
-#     "parquet.compression" = "SNAPPY"
-#   }
+  parameters = {
+    EXTERNAL              = "TRUE"
+    "parquet.compression" = "SNAPPY"
+  }
 
-#   storage_descriptor {
-#     location = "s3://${var.s3_data_bucket_name}/refined/stocks"
-#     input_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
-#     output_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"
-#     compressed = false
-#     ser_de_info {
-#       serialization_library = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
-#       parameters = {
-#         "serialization.format" = "1"
-#       }
-#     }
+  storage_descriptor {
+    location = "s3://${var.s3_data_bucket_name}/refined/stocks"
+    input_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"
+    compressed = false
+    ser_de_info {
+      serialization_library = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
+      parameters = {
+        "serialization.format" = "1"
+      }
+    }
 
-#     columns {
-#       name = "ticker"
-#       type = "string"
-#     }
+    columns {
+      name = "ticker"
+      type = "string"
+    }
 
-#     columns {
-#       name = "date"
-#       type = "string"
-#     }
+    columns {
+      name = "date"
+      type = "string"
+    }
 
-#     columns {
-#       name = "daily_volume"
-#       type = "bigint"
-#     }
+    columns {
+      name = "daily_volume"
+      type = "bigint"
+    }
 
-#     columns {
-#       name = "trade_count"
-#       type = "bigint"
-#     }
+    columns {
+      name = "average_price"
+      type = "double"
+    }
 
-#     columns {
-#       name = "average_price"
-#       type = "double"
-#     }
+    columns {
+      name = "max_high"
+      type = "double"
+    }
 
-#     columns {
-#       name = "max_high"
-#       type = "double"
-#     }
+    columns {
+      name = "min_low"
+      type = "double"
+    }
 
-#     columns {
-#       name = "min_low"
-#       type = "double"
-#     }
+    columns {
+      name = "moving_avg_7d"
+      type = "double"
+    }
 
-#     columns {
-#       name = "moving_avg_7d"
-#       type = "double"
-#     }
+    columns {
+      name = "price_change"
+      type = "double"
+    }
 
-#     columns {
-#       name = "price_change"
-#       type = "double"
-#     }
+    columns {
+      name = "price_change_pct"
+      type = "double"
+    }
+  }
 
-#     columns {
-#       name = "price_change_pct"
-#       type = "double"
-#     }
-#   }
+  partition_keys {
+    name = "process_date"
+    type = "string"
+  }
 
-#   partition_keys {
-#     name = "process_date"
-#     type = "string"
-#   }
-
-#   partition_keys {
-#     name = "ticker_partition"
-#     type = "string"
-#   }
-# }
+  partition_keys {
+    name = "ticker"
+    type = "string"
+  }
+}
