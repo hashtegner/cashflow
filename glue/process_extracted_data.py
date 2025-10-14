982a72c3ef179c8ab0852ebfa726eaf24a7e387c
Alessandro Tegner <1260049+hashtegner@users.noreply.github.com>
2025-10-13 21:18:22 -0300

    Improve glue job to write in raw table

--- glue/process_extracted_data.py
+++ glue/process_extracted_data.py
@@ -4,13 +4,13 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from pyspark.sql.functions import current_timestamp, format_date, col
+from pyspark.sql.functions import current_date, date_format
 
 ## @params: [JOB_NAME]
 args = getResolvedOptions(sys.argv, ['JOB_NAME', 'INPUT_FILE', "BUCKET_NAME"])
 input_path = args['INPUT_FILE']
 bucket_name = args['BUCKET_NAME']
-raw_data_path = f"s3://{bucket_name}/raw/market_data"
+raw_data_path = f"s3://{bucket_name}/raw/stocks"
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -23,9 +23,20 @@ df = spark.read \
             .option("header", "true") \
             .option("inferSchema", "true") \
             .csv(input_path) \
-            .withColumn("processed_at", current_timestamp()) \
-            .writeColumn("processed_date", format_date(col("processed_at"), "yyyy-MM-dd"))
+
+# add column to be used as partition key
+df = df.withColumn("process_date", date_format(current_date(), "yyyyMMdd") \
+        .cast("int"))
+
+
+# rename columns to proper names
+df = df.withColumn("name", "company_name") \
+    .withColumn("sector", "company_sector") \
+    .withColumn("retrieved_at", "retrieve_date")
 
 df.write.mode("overwrite").partitionBy("processed_date").parquet(raw_data_path)
+
+# repair table to ensure all partitions are created
+spark.sql("MSCK REPAIR TABLE cashflow.stocks_raw")
 
 job.commit()
--- tf/app/glue.tf
+++ tf/app/glue.tf
@@ -13,3 +13,103 @@ resource "aws_glue_job" "cashflow_process_extracted_data" {
     python_version = "3.9"
   }
 }
+
+
+resource "aws_glue_catalog_database" "cashflow" {
+  name = "cashflow"
+}
+
+resource "aws_glue_catalog_table" "cashflow_stocks" {
+  name = "stocks_raw"
+  database_name = aws_glue_catalog_database.cashflow.name
+
+  table_type = "EXTERNAL_TABLE"
+
+  parameters = {
+    EXTERNAL              = "TRUE"
+    "parquet.compression" = "SNAPPY"
+  }
+
+  storage_descriptor {
+    location = "s3://${var.s3_data_bucket_name}/raw/stocks"
+    input_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"
+    compressed = false
+    ser_de_info {
+      serialization_library = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
+      parameters = {
+        "serialization.format" = "1"
+      }
+    }
+
+    columns {
+      name = "company_name"
+      type = "string"
+    }
+
+    columns {
+      name = "company_sector"
+      type = "string"
+    }
+
+    columns {
+      name = "country"
+      type = "string"
+    }
+
+    columns {
+      name = "industry"
+      type = "string"
+    }
+
+    columns {
+        name = "market"
+        type = "string"
+    }
+
+    columns {
+      name = "date"
+      type = "string"
+    }
+
+    columns {
+      name = "ticker"
+      type = "string"
+    }
+
+    columns {
+        name = "open"
+        type = "double"
+    }
+
+    columns {
+        name = "close"
+        type = "double"
+    }
+
+    columns {
+        name = "high"
+        type = "double"
+    }
+
+    columns {
+        name = "low"
+        type = "double"
+    }
+
+    columns {
+        name = "volume"
+        type = "bigint"
+    }
+
+    columns {
+        name = "retrieve_date"
+        type = "string"
+    }
+  }
+
+  partition_keys {
+    name = "process_date"
+    type = "int"
+  }
+}
