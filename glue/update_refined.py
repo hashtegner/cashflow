87299df99469efc01124bdd62b55a34a4a45bf78
Alessandro Tegner <1260049+hashtegner@users.noreply.github.com>
2025-10-14 19:50:51 -0300

    Full tickers

--- glue/update_refined.py
+++ glue/update_refined.py
@@ -6,7 +6,6 @@ from awsglue.context import GlueContext
 from awsglue.job import Job
 from pyspark.sql.functions import sum, avg, max, min, col, lag
 from pyspark.sql.window import Window
-import boto3
 
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 raw_data_path = "s3://cashflow-data/raw/stocks"
@@ -50,6 +49,3 @@ df = df.withColumn("price_lag_3_month", lag("average_price", 3).over(window_full
 df.write.mode("overwrite").partitionBy("reference_date", "ticker").parquet(refined_data_path)
 
 job.commit()
-
-glue_client = boto3.client('glue')
-glue_client.sql("MSCK REPAIR TABLE cashflow.stocks_refined")
